ec3fba3914e9f90dfd61cdc6f4cd7e5e40eb1082
Facundo Beltramo <facundo.beltramo@globant.com>
2019-04-16 19:23:51 -0300

    fix code port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.APP_PORT || 8080;
+const port = 8080;
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
